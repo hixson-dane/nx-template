89625c8911d9d54a75d9e6457f1b21fb5d0c5b27
Dane Hixson <dhixson@lenovo.com>
2026-08-28 10:33:13 -0600

    removed test project

--- packages/resources/schema/schema-models/README.md
+++
@@ -1,7 +0,0 @@
-# schema-models
-
-This library was generated with [Nx](https://nx.dev).
-
-## Building
-
-Run `nx build schema-models` to build the library.
--- packages/resources/schema/schema-sdk/src/features/ping/models.ts
+++
@@ -1,3 +0,0 @@
-// GitHub Copilot generated code - start
-export type { PingRequest, PingResponse } from '@org/schema-models';
-// GitHub Copilot generated code - end
